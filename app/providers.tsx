33724326531898daf080a1be9c61215fe79c5947
Latex <M.Hassan17877@student.aast.edu>
2025-03-09 22:48:09 +0200

    Add providers component

---
+++ app/providers.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { SessionProvider } from 'next-auth/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { useState } from 'react';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </SessionProvider>
+    </QueryClientProvider>
+  );
+}
